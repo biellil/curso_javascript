9c568b7394ffc7d70063ea2b62634747246b3ccf
biel.lil <biel1313biel@gmail.com>
2023-01-17 19:31:54 -0300

    Update evento.js

--- eventosJavaScript/evento.js
+++ eventosJavaScript/evento.js
@@ -1,4 +1,4 @@
-/*window.addEventListener('focus',event =>{
+window.addEventListener('focus',event =>{
 
    console.log('focus');
     
@@ -7,7 +7,7 @@
 document.addEventListener('click',event =>{
    console.log('click');
 });
-*/
+
 let agora = new Date();
 console.log(agora.getDate(),agora.getMonth(),agora.getFullYear());
 console.log(agora.toLocaleDateString('pr-br'));
